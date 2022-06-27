650e40f074177a110819ecf87581114aa9cb4165
Alla Siki <alla@terasky.com>
2022-06-27 14:19:33 +0300

    change amount of nodes to 1 due to quata limit

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   default_node_pool {
     name            = "default"
-    node_count      = 2
+    node_count      = 1
     vm_size         = "Standard_DS2_V2"
     os_disk_size_gb = 30
   }
