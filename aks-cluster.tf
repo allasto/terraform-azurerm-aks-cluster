bfec757b6771632d0e914d66094b049e10b99624
Alla Siki <alla@terasky.com>
2022-06-27 14:08:08 +0300

    change size to DS2_V2

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_B1s"
+    vm_size         = "Standard_DS2_V2"
     os_disk_size_gb = 30
   }
 
